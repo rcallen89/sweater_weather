c08f0aad414b5508144c8ea54ff34db98604b695
Ryan Allen <rcallen89@gmail.com>
2020-04-19 11:09:44 -0500

    Add Open Weather Service with Spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,18 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.ignore_localhost = true
+  config.default_cassette_options = { record: :new_episodes }
+  config.filter_sensitive_data('<OPEN_WEATHER_ONE>') { ENV['OPEN_WEATHER_ONE'] }
+  config.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
