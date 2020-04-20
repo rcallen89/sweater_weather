5ae5907e1406654c8262504af9506a476f5db22a
Ryan Allen <rcallen89@gmail.com>
2020-04-20 11:31:14 -0500

    Remove Pry from test

--- spec/requests/api/v1/antipode_request_spec.rb
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe 'Antipode API Request', type: :request do
     location = {location: "Hong Kong"}
 
     get "/api/v1/antipode", params: location
+    parsed = JSON.parse(response.body, symbolize_names: true)
 
-    require 'pry'; binding.pry
+    expect(parsed[:data][:attributes]).to have_key(:location_name)
+
+    expect(parsed[:data][:attributes]).to have_key(:forecast)
+    expect(parsed[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(parsed[:data][:attributes][:forecast]).to have_key(:current_temperature)
+
+    expect(parsed[:data][:attributes]).to have_key(:search_location)
   end
 end
