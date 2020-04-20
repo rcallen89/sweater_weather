214e9b2e05848b00c102155d6deb867905b7f404
Ryan Allen <rcallen89@gmail.com>
2020-04-20 09:23:52 -0500

    Add background PORO and serializer

---
+++ app/models/background.rb
@@ -0,0 +1,13 @@
+class Background
+  attr_reader :image_url, :id
+
+  def initialize(location)
+    @id = nil
+    @image_url = get_image_url(location)
+  end
+
+  def get_image_url(location)
+    BackgroundService.get_image(location)[:results][0][:urls][:raw]
+  end
+
+end
