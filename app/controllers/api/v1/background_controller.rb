7b9ff8d4e4d334e606b9746e5ed99988cca569a1
Ryan Allen <rcallen89@gmail.com>
2020-04-21 11:26:34 -0500

    Fixed Caching

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V1::BackgroundController < ApplicationController
   def show
-    Rails.cache.fetch("background_for_#{params[:location]}", expires: 24.hours) do
+    Rails.cache.fetch("background_for_#{params[:location]}", expires_in: 24.hours) do
       render json: BackgroundSerializer.new(Background.new(params[:location]))
     end
   end
