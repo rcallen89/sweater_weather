72acd24dc0b2a3cb191894a515372ceaad3e5ebe
Ryan Allen <rcallen89@gmail.com>
2020-04-21 11:47:59 -0500

    Remove Cacheing

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -2,8 +2,6 @@
 
 class Api::V1::BackgroundController < ApplicationController
   def show
-    Rails.cache.fetch("background_for_#{params[:location]}", expires_in: 24.hours) do
-      render json: BackgroundSerializer.new(Background.new(params[:location]))
-    end
+    render json: BackgroundSerializer.new(Background.new(params[:location]))
   end
 end
